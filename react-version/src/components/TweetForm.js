805a2b3d31cec81efdab0dc890adc09bf85e9f63
David Figueroa <figueroadavid93@gmail.com>
2023-01-05 13:28:44 -0500

    React based Tweeter project completed

--- react-version/src/components/TweetForm.js
+++ react-version/src/components/TweetForm.js
@@ -6,13 +6,14 @@ function TweetForm() {
   
   return (
     <section className="newtweet">
-      <form method="post" action="/tweets" className="newtweet__form">
+      <form method="post" action="/tweets" className="newtweet__form" onSubmit={(event) => event.preventDefault()}>
         <textarea className="form__textarea" name="text" placeholder={tweetFormText}></textarea>
         <input type="submit" value="Tweet" className="form__input" />
           <span className="form__counter">140</span>
       </form>
     </section>
   );
+  
 }
 
 export default TweetForm;
